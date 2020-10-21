8444cbaa59527583543a9a3b9bfa53484f40dc3d
Yaser Ahmed <yaserfahmed@gmail.com>
2020-10-20 20:53:31 -0700

    add files

---
+++ Pelican/content/pages/home.md
@@ -0,0 +1,11 @@
+Title: Home
+Date: 2020-10-20 10:20
+URL:
+save_as: index.html
+
+Following is a review of my favorite mechanical keyboard.
+
+| Syntax      | Description |
+| ----------- | ----------- |
+| Header      | Title       |
+| Paragraph   | Text        |
---
+++ Pelican/pelicanconf.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = 'Yaser Ahmed'
+SITENAME = 'yprime'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'America/Los_Angeles'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('yprime', 'https://trade.yprime.xyz/'),
+         ('Github', 'https://github.com/yahmed153/'))
+
+# Social widget
+SOCIAL = (('Another social link', '#'), ('Another social link', '#'))
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
